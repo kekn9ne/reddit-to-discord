43461dc4e8ba792d60c71be2e3e87b4c2b65eeaf
Aytuğ <kekn9ne@yandex.com>
2021-01-15 00:02:39 +0300

    Video support

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Reddit-To-Discord
 
 Reddit-To-Discord bot gets hot posts from specified subreddits and sends them to Discord servers using webhook. It's pretty simple to use it.
@@ -7,5 +8,6 @@ Reddit-To-Discord bot gets hot posts from specified subreddits and sends them to
 * Instal requirements by typing ``pip install -r requirements.txt``.
 * Edit `config.py` file.
 * Run the bot.
+#### If you are on Linux or MacOS you need to install FFmpeg. If you are on Windows, instructions above will be enough.
 
 [Video demonstration & usage](https://youtu.be/KJgSVB797NY)
--- reddit_to_discord.py
+++ reddit_to_discord.py
@@ -4,6 +4,9 @@ import os
 import requests
 import json
 import config
+import ffmpeg
+from urllib.request import urlretrieve
+from discord_webhook import DiscordWebhook
 
 def bot_login():
     print("Loggin in.")
@@ -16,55 +19,96 @@ def run_bot(r):
         submissions = r.subreddit(sub).hot(limit = config.limit)
         for submission in submissions:
             if submission not in blacklist:
-                if submission.is_self == False and submission.is_video == False:                    
-                    data = {}
-                    data["embeds"] = []
-                    embed = {
-                            "title": config.embed_title,
-                            "author": {
-                                "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
-                                "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
-                                "icon_url": submission.author.icon_img
-                            },
-                            "image": {
-                                "url": submission.url
-                            },
-                            "fields": [
-                                {
-                                    "name": "Post Title",
-                                    "value": submission.title,
-                                    "inline": False
-                                },
-                                {
-                                    "name": "Author",
-                                    "value": "u/{}".format(str(submission.author)),
-                                    "inline": True
-                                }, 
-                                {
-                                    "name": "Subreddit",
-                                    "value": "r/{}".format(str(submission.subreddit)),
-                                    "inline": True
-                                },
-                                {
-                                    "name": "Score",
-                                    "value": submission.score,
-                                    "inline": False
-                                }
-                            ]
-                        }
-                    data["embeds"].append(embed)
+                if submission.is_self == False:
+                    if submission.is_video == True:
+                        video = submission.media['reddit_video']['fallback_url']
+                        audio = ""
+                        if ("_480." in video):
+                            audio = video.replace("480", "audio")
+                        elif ("_720." in video):
+                            audio = video.replace("720", "audio")
+                        elif ("_1080." in video):
+                            audio = video.replace("1080", "audio")
+                        else:
+                            print("Found a video but it's resolution not supported.")
+                            return
 
-                    req = requests.post(config.webhook_url, data = json.dumps(data), headers={"Content-Type": "application/json"})
+                        urlretrieve(video, str(submission) + ".mp4")
+                        urlretrieve(audio, str(submission) + ".mp3")
+
+                        input_video = ffmpeg.input(str(submission) + '.mp4')
+
+                        input_audio = ffmpeg.input(str(submission) + '.mp3')
+
+                        ffmpeg.concat(input_video, input_audio, v=1, a=1).output(str(submission) + '_merged.mp4').global_args('-loglevel', 'error').run()
+                        
+                        webhook = DiscordWebhook(url=config.webhook_url, content="**{}**\nPosted by ``u/{}`` at ``r/{}``\nTitle: ``{}``".format(config.embed_title, str(submission.author), str(submission.subreddit), str(submission.title)))
+                        with open(str(submission) + "_merged.mp4", "rb") as f:
+                            webhook.add_file(file=f.read(), filename=submission.title + ".mp4")
+
+                        response = webhook.execute()
+                        
+                        print("Sent a video to the Discord server.")
+
+                        os.remove(str(submission) + ".mp3")
+                        os.remove(str(submission) + ".mp4")
+                        os.remove(str(submission) + "_merged.mp4")
+
+                        blacklist.append(submission)
+                        with open("blacklist.txt", "a") as f:
+                            f.write(str(submission) + "\n")
+                            f.close()
+
+                        time.sleep(config.wait_time)
+                    else:
+                        data = {}
+                        data["embeds"] = []
+                        embed = {
+                                "title": config.embed_title,
+                                "author": {
+                                    "name": "posted by u/{} at r/{}".format(str(submission.author), str(submission.subreddit)),
+                                    "url": "https://reddit.com/r/{}/comments/{}".format(str(submission.subreddit), str(submission)),
+                                    "icon_url": submission.author.icon_img
+                                },
+                                "image": {
+                                    "url": submission.url
+                                },
+                                "fields": [
+                                    {
+                                        "name": "Post Title",
+                                        "value": submission.title,
+                                        "inline": False
+                                    },
+                                    {
+                                        "name": "Author",
+                                        "value": "u/{}".format(str(submission.author)),
+                                        "inline": True
+                                    }, 
+                                    {
+                                        "name": "Subreddit",
+                                        "value": "r/{}".format(str(submission.subreddit)),
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Score",
+                                        "value": submission.score,
+                                        "inline": False
+                                    }
+                                ]
+                            }
+                        data["embeds"].append(embed)
+
+                        req = requests.post(config.webhook_url, data = json.dumps(data), headers={"Content-Type": "application/json"})
+                        
+                        if req.status_code == 204:
+                            print("Sent an image to the Discord server.")
                     
-                    if req.status_code == 204:
-                        print("Found new hot Reddit post, sending it to Discord.")
-                
-                    blacklist.append(submission)
-                    with open("blacklist.txt", "a") as f:
-                        f.write(str(submission) + "\n")
-                        f.close()
+                        blacklist.append(submission)
+                        with open("blacklist.txt", "a") as f:
+                            f.write(str(submission) + "\n")
+                            f.close()
 
-                    time.sleep(config.wait_time)
+                        time.sleep(config.wait_time)
 
     
 
